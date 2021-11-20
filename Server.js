d5577071c07f93c78e920e21985f84da3c69db50
clansymetron <60947008+clansymetron@users.noreply.github.com>
2021-11-20 02:20:19 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,12 +1,16 @@
 http = require('http');
+//const express = require ('express');
 var mysql = require('mysql');
 const url = require ('url');
+require('dotenv').config();
+var a =0;
+var timetablee =0;
+var tim=0;
 //const d = require('dateformat'); descargar
 
-
-const hostname = "10.192.151.163";
-const port = 4344;
-var user= 'Max Lampurlanés'; //Contemplo que ja esta iniciat sessió
+const hostname = "172.20.10.2";
+const port =  4344;
+var user= ''; //Contemplo que ja esta iniciat sessió
 
 function manage( req, res){  //funció que gestiona les peticions de clients
 if(req.method =='GET'){
@@ -20,6 +24,14 @@ if(req.method =='GET'){
    });
 
    req.on('end', ()=>{   //tota la funcionalitat es troba en quan acaba la petició
+
+    var disc = req.url.split("?");
+    if(disc[1]=="d"){
+    user="";
+    res.end("disconnected");
+    }
+    else{
+
      if(user==''){
       var stringname;
       var uid = req.url.split("?"); //separo el url per obtenir el uid
@@ -27,18 +39,23 @@ if(req.method =='GET'){
       console.log(qq);
       con.query(qq, function(err,result){  //consulta SQL
       if(err) throw err;
+
       stringname = JSON.stringify(result); //passem a string el objecte
-      //final = JSON.parse(striing); //pasem a JSON el string
-      //user = final[0].name; //agafem del Json la row 0 i el name, que sera usuari actual
+      console.log(stringname);
+if(stringname=="[]") res.end("error");
+else{
+      final = JSON.parse(stringname); //pasem a JSON el string
+      console.log(final);
+      user = final[0].name.toString(); //agafem del Json la row 0 i el name, que sera usuari actual
       res.statusCode=200;
-      res.setHeader('Content_Type','aplication/json');
+      //res.setHeader('Content_Type','aplication/json');
 
       //var baina = "'"+ stringname + "'";
-      console.log(stringname);
+
       var data= "{"+ '"result":' + stringname + "}";
-      res.end(data);
+      res.end(user);
 
-
+}
 
        });
 
@@ -49,11 +66,12 @@ else{
      var url = req.url.split("?");
       var all = 1;
       var lim= 0;
-     console.log(url[1]);
+     console.log(url);
 
      if(url.length>=3){ //comprovem si te restriccions
       all = 0;
      var params = url[2].split("&");
+     console.log(params);
    }
    console.log(all);
      var query = "SELECT ";
@@ -62,11 +80,21 @@ else{
      switch(url[1]){ //mirem quina taula és
        case 'marks':
        query+="subject, name, mark";
-       querfinal=" ORDER BY mark DESC";
+       querfinal=" ORDER BY subject ASC";
        break;
        case 'timetable': //"day , hour, subject, room";//
-        query += "room, subject, hour, day";
-       break;
+        query += "day, hour, subject, room";
+
+        var date = new Date();
+        var hour =  date.toTimeString().substr(0,5);
+         //get only houy;
+         var dayint = date.getDay() -1;
+       //dayint = 2;
+        if(dayint>4) dayint = 0;
+
+        querfinal = "ORDER BY CASE when daynum = "+ dayint +  " and hour >= '" + hour +"' then 0 when daynum = "+ (dayint+1)%5 +" then 1 when daynum= "+ (dayint+2)%5 +" then 2 when daynum= "+ (dayint+3)%5 +" then 3 when daynum= "+ (dayint+4)%5 +" then 4 when daynum";
+        querfinal += "= " + dayint +" AND hour< '" + hour + "' then 5 end";
+        break;
        case 'tasks': query += "date, subject, name";
        querfinal=" ORDER BY date ASC";
        break;
@@ -74,9 +102,10 @@ else{
        break;
        default: break;
      }
-    query+= " FROM "+ url[1] + " WHERE student = '" + user + "'";; //creem query SQL
 
-if(all!=1){ //mirem resta de restriccions!!!
+    query+= " FROM "+ url[1] + " WHERE student = '" + user + "'"; //creem query SQL
+
+    if(all!=1){ //mirem resta de restriccions!!!
     for(const element of params){
      var p = element.split("=");
       //comprobar que no sea limit
@@ -100,7 +129,8 @@ if(all!=1){ //mirem resta de restriccions!!!
            break;
            case 'date':
            //'date':  var date= new Date(p[1]);
-           query+= " AND date "  + " = '" + p[1]+ "'";
+           query+= " AND date "  + ' = "' + p[1]+ '"';
+          // console.log(p[1]);
            break;
            case 'day': query+= " AND " + p[0] + " = '" + p[1] + "'";
            break;
@@ -124,16 +154,18 @@ if(all!=1){ //mirem resta de restriccions!!!
 
            case 'date[gte]': query+= " AND date" + " >= DATE '";
            if(p[1]=="now"){
-             //const now = new Date();
-            // query+= now.format(dd/mm/aa)
+             var now = new Date();
+              query += now.toISOString().split('T')[0] + "'";
            }else{
              query+= p[1]+"'";
            }
            break;
            case 'date[lt]': query+= " AND  date" + " < '";
            if(p[1]=="now"){
-             //const now = new Date();
-            // query+= now.format(dd/mm/aa)
+             var now = new Date();
+
+             query += now.toISOString().split('T')[0] + "'";
+
            }else{
              query+= p[1]+ "'";
            }
@@ -145,29 +177,50 @@ if(all!=1){ //mirem resta de restriccions!!!
        }
 
   }
-  if(lim!=1)  query+=querfinal;
 
+  //  if(timetablee==1){
+
+  //  if(lim==1) query += " limit " + params[0].split("=")[1];
+
+  //  tim =1;
+//  }
+//  }
+
+  //if(lim!=1 && tim==0) {
+    //console.log("entroooo");
+    //query+=querfinal;
+    //tim = 1;
+  //}
+
+if(lim!=1 )   query+=querfinal;
 
   console.log(query);
 
-
+    timetablee = 0;
     var striing
      con.query(query, function(err,result){ //SQL consulta
-     if(err) throw err;
+     if(err) res.end("error");
+     else{
+     //console.log(result);
      striing = JSON.stringify(result);
+
      //   var body = JSON.parse(striing);
-       console.log(striing[1]);
+
        res.statusCode=200;
        res.setHeader('Content_Type','aplication/json');
        //res.end(striing);
        var data= "{"+ '"result":' + striing + "}";
+    //   console.log(striing);
+       console.log(data);
        res.end(data);
+}
      });
 
-
-    ///  res.write(striing);
+    // res.write(striing);
        //devolvemos el JSON al cliente
    }
+
+}
   });
 
 
@@ -182,21 +235,24 @@ if(all!=1){ //mirem resta de restriccions!!!
 //starting server//
 
 const server = http.createServer(manage);
-
+//const app = express();
 
 server.listen (port, hostname, () =>{
   console.log('Server running at http://%s:%d',hostname,port)
 });
+//app.listen (port,() =>{
+  //console.log('Server running at http://%d',port)
+//});
 
 
 //mySQL connection
 
 
 var con = mysql.createConnection({
-host: 'localhost',
-database: 'PBE',
-user: 'root',
-password : 'Alex12345'
+host: process.env.DB_HOST || 'localhost',
+database: process.env.DB_DATABASE || 'PBE',
+user: process.env.DB_USER|| 'root',
+password : process.env.DB_PASSWORD ||'Alex12345'
 });
 
 con.connect(function(err){
